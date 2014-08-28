534e94b4c1ec3ee239a83d6a4f36bcc30671b7d4
unknown <oivasitc@chclass16.training.local>
2014-08-28 17:24:40 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//var lettersRegex = /^[A-Za-z]+$/; 
+
 var ValidationModel = Backbone.Model.extend({
 	defaults: function(){
 		return{
@@ -8,63 +8,45 @@ var ValidationModel = Backbone.Model.extend({
 			resultOfValidation: ""
 		}
 	},
-	clear: function(){
-		this.set({userName: "",userLogin: "", categoriesCount: 0, resultOfValidation: ""});
-	},
-	checkUserName: function(){
-		//console.log(this.get('userName'));
-		if(this.get('userName').length >= 1){
-			console.log(this.get('userName').charAt(0));
-			if( !(this.get('userName').charAt(0).match("/^[A-Z]$/"))){
-				return "Wrong user name";
-			}
-			if(this.get('userName').match("/\s+/")){
-				return "Wrong user name";
-			}
-			return "";
+
+	validate:function(attr){
+		var PATTERN_USERNAME = /^[A-Z][a-zA-Z]*$/;
+		var PATTERN_USERLOGIN = /^[A-Za-z0-9_]*$/;
+		if ((attr.userName.search(PATTERN_USERNAME) == -1) || (attr.userName.length <= 1)) {
+			this.set('resultOfValidation', "Wrong user name. ");
 		}
-		else{ return "Wrong user name";}
-	},
-	checkUserLogin: function(){
-		if(this.get('userLogin').length >= 1){
-			if(this.get('userLogin').match("/\s+/")){
-				return "Wrong user login";
-			}
-			return "";
+		if ((attr.userLogin.search(PATTERN_USERLOGIN) == -1) || (attr.userLogin.length <= 1)) {
+			this.set('resultOfValidation', this.get('resultOfValidation')+"Wrong user login. ");
 		}
-		else {return "Wrong user login";}
-	},
-	checkCategoriesCount: function(){
-		//console.log(this.get('categoriesCount'));
-		if(this.get('categoriesCount') > 5){
-			return "Wrong count of categories";
-		}
-		else return "";
+
 	}
+
 });
 
 var ValidationView = Backbone.View.extend({
 	el: $('#messageFromScript'),
 	template: _.template($('#resultField').html()),
+	initialize:function(){
+		$("#check").on('click',$.proxy(this.setData,this))
+	},
+	setData:function(){
+		this.model.set('resultOfValidation',"");
+		this.model.set({'userName': $('#userName').val()});
+		this.model.set({'userLogin': $('#userLogin').val()},{validate:true});
 
-	render: function(){
-		this.model.clear();
 		var categoriesCollection = $('.categories');
 		for(var elementIndex = 0, countOfElements = categoriesCollection.length; elementIndex<countOfElements; elementIndex++){
 			if(categoriesCollection[elementIndex].checked){
 				this.model.set('categoriesCount', this.model.get('categoriesCount')+1);
 			}
 		}
-		this.model.set('userName', $('#userName').val());
-		this.model.set('userLogin', $('#userLogin').val());
-		console.log(this.model.get('categoriesCount'));
-		//console.log(this.model.get('userName'));
-		//console.log(this.model.get('userLogin'));
-		this.model.set('resultOfValidation', this.model.checkUserName() +" "+this.model.checkUserLogin()+" "+this.model.checkCategoriesCount());
-
+		if(this.model.get('categoriesCount') > 5){
+			this.model.set('resultOfValidation', this.model.get('resultOfValidation')+"Wrong categories count.");
+		}
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
 	}
+
 });
 
 var validation = new ValidationView({model: new ValidationModel});
